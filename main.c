de2bced14d39c4b603a755aab44c87083ca75c38
Asplushi_ <veis2k15veis@gmail.com>
2024-06-19 12:14:21 +0200

    basic maze generator

--- main.c
+++ main.c
@@ -1,19 +1,41 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SCREEN_WIDTH 600
+#define SCREEN_HEIGHT 600
+#define CELL_SIZE 20
+#define GRID_WIDTH (SCREEN_WIDTH / CELL_SIZE)
+#define GRID_HEIGHT (SCREEN_HEIGHT / CELL_SIZE)
+
+typedef enum { UP, DOWN, LEFT, RIGHT } Direction;
+
+typedef struct {
+    int x, y;
+    int visited;
+    int walls[4]; 
+} Cell;
+
+void initGrid(Cell grid[GRID_WIDTH][GRID_HEIGHT]);
+void removeWalls(Cell *current, Cell *next);
+void drawGrid(SDL_Renderer *renderer, Cell grid[GRID_WIDTH][GRID_HEIGHT]);
+Cell* getNeighbour(Cell grid[GRID_WIDTH][GRID_HEIGHT], Cell *current);
 
 int main(int argc, char* argv[]) {
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL_Error: %s\n", SDL_GetError());
         return 1;
     }
 
     SDL_Window* window = SDL_CreateWindow(
-        "SDL",           
-        SDL_WINDOWPOS_UNDEFINED,       
-        SDL_WINDOWPOS_UNDEFINED,       
-        600,                           
-        600,                           
-        SDL_WINDOW_SHOWN               
+        "Maze",               
+        SDL_WINDOWPOS_UNDEFINED,        
+        SDL_WINDOWPOS_UNDEFINED,        
+        SCREEN_WIDTH,                   
+        SCREEN_HEIGHT,                  
+        SDL_WINDOW_SHOWN                
     );
 
     if (window == NULL) {
@@ -23,8 +45,16 @@ int main(int argc, char* argv[]) {
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    int quit = 0;
+    Cell grid[GRID_WIDTH][GRID_HEIGHT];
+    initGrid(grid);
 
+    Cell *current = &grid[0][0];
+    current->visited = 1;
+    Cell *stack[GRID_WIDTH * GRID_HEIGHT];
+    int stackSize = 0;
+    srand(time(0));
+
+    int quit = 0;
     SDL_Event e;
 
     while (!quit) {
@@ -36,10 +66,32 @@ int main(int argc, char* argv[]) {
             }
         }
 
+        Cell *next = getNeighbour(grid, current);
+        if (next != NULL) {
+            next->visited = 1;
+            stack[stackSize++] = current;
+            removeWalls(current, next);
+            current = next;
+        } else if (stackSize > 0) {
+            current = stack[--stackSize];
+        }
+
         SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
         SDL_RenderClear(renderer);
 
+        drawGrid(renderer, grid);
+
+        SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 0xFF);
+        SDL_Rect startRect = {0, 0, CELL_SIZE, CELL_SIZE};
+        SDL_RenderFillRect(renderer, &startRect);
+
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
+        SDL_Rect endRect = { (GRID_WIDTH - 1) * CELL_SIZE, (GRID_HEIGHT - 1) * CELL_SIZE, CELL_SIZE, CELL_SIZE};
+        SDL_RenderFillRect(renderer, &endRect);
+
         SDL_RenderPresent(renderer);
+
+        SDL_Delay(10); 
     }
 
     SDL_DestroyRenderer(renderer);
@@ -48,4 +100,89 @@ int main(int argc, char* argv[]) {
     SDL_Quit();
 
     return 0;
+}
+
+void initGrid(Cell grid[GRID_WIDTH][GRID_HEIGHT]) {
+    for (int x = 0; x < GRID_WIDTH; x++) {
+        for (int y = 0; y < GRID_HEIGHT; y++) {
+            grid[x][y].x = x;
+            grid[x][y].y = y;
+            grid[x][y].visited = 0;
+            grid[x][y].walls[UP] = 1;
+            grid[x][y].walls[DOWN] = 1;
+            grid[x][y].walls[LEFT] = 1;
+            grid[x][y].walls[RIGHT] = 1;
+        }
+    }
+}
+
+void removeWalls(Cell *current, Cell *next) {
+    int dx = next->x - current->x;
+    int dy = next->y - current->y;
+
+    if (dx == 1) {
+        current->walls[RIGHT] = 0;
+        next->walls[LEFT] = 0;
+    } else if (dx == -1) {
+        current->walls[LEFT] = 0;
+        next->walls[RIGHT] = 0;
+    }
+
+    if (dy == 1) {
+        current->walls[DOWN] = 0;
+        next->walls[UP] = 0;
+    } else if (dy == -1) {
+        current->walls[UP] = 0;
+        next->walls[DOWN] = 0;
+    }
+}
+
+void drawGrid(SDL_Renderer *renderer, Cell grid[GRID_WIDTH][GRID_HEIGHT]) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0xFF);
+
+    for (int x = 0; x < GRID_WIDTH; x++) {
+        for (int y = 0; y < GRID_HEIGHT; y++) {
+            int x1 = x * CELL_SIZE;
+            int y1 = y * CELL_SIZE;
+            int x2 = x1 + CELL_SIZE;
+            int y2 = y1 + CELL_SIZE;
+
+            if (grid[x][y].walls[UP]) {
+                SDL_RenderDrawLine(renderer, x1, y1, x2, y1);
+            }
+            if (grid[x][y].walls[DOWN]) {
+                SDL_RenderDrawLine(renderer, x1, y2, x2, y2);
+            }
+            if (grid[x][y].walls[LEFT]) {
+                SDL_RenderDrawLine(renderer, x1, y1, x1, y2);
+            }
+            if (grid[x][y].walls[RIGHT]) {
+                SDL_RenderDrawLine(renderer, x2, y1, x2, y2);
+            }
+        }
+    }
+}
+
+Cell* getNeighbour(Cell grid[GRID_WIDTH][GRID_HEIGHT], Cell *current) {
+    Cell *neighbours[4];
+    int n = 0;
+
+    if (current->y > 0 && !grid[current->x][current->y - 1].visited) {
+        neighbours[n++] = &grid[current->x][current->y - 1];
+    }
+    if (current->y < GRID_HEIGHT - 1 && !grid[current->x][current->y + 1].visited) {
+        neighbours[n++] = &grid[current->x][current->y + 1];
+    }
+    if (current->x > 0 && !grid[current->x - 1][current->y].visited) {
+        neighbours[n++] = &grid[current->x - 1][current->y];
+    }
+    if (current->x < GRID_WIDTH - 1 && !grid[current->x + 1][current->y].visited) {
+        neighbours[n++] = &grid[current->x + 1][current->y];
+    }
+
+    if (n > 0) {
+        return neighbours[rand() % n];
+    } else {
+        return NULL;
+    }
 }
